1fb2f37f2dfdef9369d88bc0ea8119fa1b4c9505
Dipika Mahashabde <dipika.2204@csu.fullerton.edu>
2016-03-07 22:56:45 -0800

    Assignment no 3 modified

--- homework3.js
+++ homework3.js
@@ -1,56 +1,7 @@
-var oneparameter = function (input, fname) {
-    var num = document.getElementById(input).value;
-    var temp = [];
-    temp = num.split(",");
-    var a;
-    for (a = 0; a < temp.length; a++) {
-        temp[a] = parseInt(temp[a], 10);
-    }
-    switch (fname) {
-        case 'exercise1':
-            document.getElementById("add_output1").innerHTML = exercise1(temp).toString();
-            break;
-        case 'exercise2':
-            document.getElementById("add_output2").innerHTML = exercise2(temp);
-            break;
-        case 'exercise3':
-            document.getElementById("add_output3").innerHTML = exercise3(temp).toString();
-            break;
-        case 'exercise4':
-            document.getElementById("add_output4").innerHTML = exercise4(temp).toString();
-            break;
-        case 'max':
-            document.getElementById("add_output8").innerHTML = max(temp);
-            break;
-        case 'oneeven':
-            document.getElementById("add_output9").innerHTML = oneeven(temp);
-            break;
-        case 'alleven':
-            document.getElementById("add_output10").innerHTML = alleven(temp);
-            break;
-        default:
-            alert("Wrong input");
-    }
-
-};
-
-var twoparameter = function (input1, input2, fname) {
-    var num = document.getElementById(input1).value;
-    var num1 = document.getElementById(input2).value;
-    var temp = [];
-    temp = num.split(",");
-    switch (fname) {
-        case 'arrayContains':
-            document.getElementById("add_output5").innerHTML = arrayContains(temp, num1).toString();
-            break;
-        case 'arrayContainsTwo':
-            document.getElementById("add_output6").innerHTML = arrayContainsTwo(temp, num1).toString();
-            break;
-        default:
-            alert("Wrong input");
-    }
-
-};
+"use strict";
+/*jshint browser:true*/
+/*globals _*/
+/*jshint unused:false*/
 
 //1st question
 var exercise1 = function (num) {
@@ -94,9 +45,8 @@ var exercise4 = function (num) {
 //in
 var arrayContains = function (str1, str2) {
     for (var i = 0; i < 6; i++) {
-        if (str1[i] == str2) {
+        if (str1[i] === str2) {
             return true;
-
         }
     }
     return false;
@@ -106,7 +56,7 @@ var arrayContains = function (str1, str2) {
 var arrayContainsTwo = function (str8, str9) {
     var count = 0;
     for (var i = 0; i < 6; i++) {
-        if (str8[i] == str9) {
+        if (str8[i] === str9) {
             count++;
         }
     }
@@ -127,21 +77,19 @@ var arrayContainsNTimes = function (str3, str4, count) {
     var temp = [];
     temp = num.split(",");
     var count2 = 0;
-    for (var i = 0; i < 6; i++) {
-        if (temp[i] == num1) {
+    for (var i = 0; i < temp.length; i++) {
+        if (temp[i] === num1) {
             count2++;
         }
     }
-    if (count1 == count2) {
-        document.getElementById("add_output7").innerHTML = ('True');
+    if (count1 <= count2) {
+        document.getElementById("add_output7").innerHTML = ("True");
     }
     else {
-        document.getElementById("add_output7").innerHTML = ('False');
+        document.getElementById("add_output7").innerHTML = ("False");
     }
 
 };
-
-
 
 //underscore:
 //max
@@ -159,4 +107,60 @@ var oneeven = function (num1) {
 var alleven = function (num1) {
     var all_even = _.every(num1, function (num) { return num % 2 === 0; });
     return all_even;
+};
+
+
+var oneparameter = function (input, fname) {
+    var num = document.getElementById(input).value;
+    var temp = [];
+    temp = num.split(",");
+    var a;
+    for (a = 0; a < temp.length; a++) {
+        temp[a] = parseInt(temp[a], 10);
+    }
+    switch (fname) {
+        case "exercise1":
+            document.getElementById("add_output1").innerHTML = exercise1(temp).toString();
+            break;
+        case "exercise2":
+            document.getElementById("add_output2").innerHTML = exercise2(temp);
+            break;
+        case "exercise3":
+            document.getElementById("add_output3").innerHTML = exercise3(temp).toString();
+            break;
+        case "exercise4":
+            document.getElementById("add_output4").innerHTML = exercise4(temp).toString();
+            break;
+        case "max":
+            document.getElementById("add_output8").innerHTML = max(temp);
+            break;
+        case "oneeven":
+            document.getElementById("add_output9").innerHTML = oneeven(temp);
+            break;
+        case "alleven":
+            document.getElementById("add_output10").innerHTML = alleven(temp);
+            break;
+        default:
+            window.alert("Wrong input");
+    }
+
+};
+
+
+var twoparameter = function (input1, input2, fname) {
+    var num = document.getElementById(input1).value;
+    var num1 = document.getElementById(input2).value;
+    var temp = [];
+    temp = num.split(",");
+    switch (fname) {
+        case "arrayContains":
+            document.getElementById("add_output5").innerHTML = arrayContains(temp, num1).toString();
+            break;
+        case "arrayContainsTwo":
+            document.getElementById("add_output6").innerHTML = arrayContainsTwo(temp, num1).toString();
+            break;
+        default:
+            window.alert("Wrong input");
+    }
+
 };
